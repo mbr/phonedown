a9f5ff21b7d9ace9469f1ccf92bb6583f97d8846
Marc Brinkmann <git@marcbrinkmann.de>
2016-08-22 14:09:42 +0200

    YaPF.

--- phonedown.py
+++ phonedown.py
@@ -13,7 +13,6 @@ from mutagen.flac import FLAC
 from mutagen import File
 import progressbar
 
-
 status_queue = Queue()
 
 
@@ -26,16 +25,20 @@ def folder(v):
 
     return os.path.abspath(v)
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('source_folder', type=folder)
 parser.add_argument('dest_folder', type=folder)
 parser.add_argument('-e', '--extensions', nargs='+', default=['.flac'])
-parser.add_argument('-p', '--pool-size', type=int,
-                    default=1 + cpu_count())
+parser.add_argument('-p', '--pool-size', type=int, default=1 + cpu_count())
 parser.add_argument('lame_options', nargs=argparse.REMAINDER)
-parser.add_argument('--no-mp3gain', action='store_false', dest='apply_mp3gain',
+parser.add_argument('--no-mp3gain',
+                    action='store_false',
+                    dest='apply_mp3gain',
                     default=True)
-parser.add_argument('--no-skip', action='store_false', dest='skip_existing',
+parser.add_argument('--no-skip',
+                    action='store_false',
+                    dest='skip_existing',
                     default=True)
 parser.add_argument('--lame', default='lame')
 parser.add_argument('--flac', default='flac')
@@ -64,12 +67,13 @@ def convert_file(args, full_path):
             if e.errno != 17:
                 raise  # only raise if not "file exists" error
 
-        flac_p = subprocess.Popen([args.flac, '-s', '-d', '--stdout',
-                                  full_path],
-                                  stdout=subprocess.PIPE,
-                                  preexec_fn=ignore_sigint)
+        flac_p = subprocess.Popen(
+            [args.flac, '-s', '-d', '--stdout', full_path],
+            stdout=subprocess.PIPE,
+            preexec_fn=ignore_sigint)
         lame_cmd = [args.lame] + lame_options + ['--quiet', '-', out_path]
-        lame_p = subprocess.Popen(lame_cmd, stdin=flac_p.stdout,
+        lame_p = subprocess.Popen(lame_cmd,
+                                  stdin=flac_p.stdout,
                                   preexec_fn=ignore_sigint)
 
         flac_p.wait()
@@ -83,8 +87,8 @@ def convert_file(args, full_path):
 
         # finally, correct the tags
         id3data = File(out_path, easy=True)
-        for attr in ('title', 'artist', 'album', 'date',
-                     'genre', 'tracknumber'):
+        for attr in ('title', 'artist', 'album', 'date', 'genre',
+                     'tracknumber'):
             id3data[attr] = metadata.get(attr)
         id3data.save()
     except Exception, e:
@@ -137,21 +141,19 @@ if __name__ == '__main__':
         print "Nothing to be done"
         sys.exit(0)
 
-    print "Using %d workers to process %.2fM" % (
-        args.pool_size,
-        total_size / (1024 * 1024)
-    )
+    print "Using %d workers to process %.2fM" % (args.pool_size,
+                                                 total_size / (1024 * 1024))
 
     pool = Pool(args.pool_size, ignore_sigint)
 
     try:
         res = pool.map_async(_convert_file_args, list_files(args))
-        pbar = progressbar.ProgressBar(
-        widgets=[
-            'Encoding: ', progressbar.AnimatedMarker(),
-            progressbar.Percentage(), ' ', progressbar.Bar(marker='*'),
-            ' ', progressbar.ETA(), ' ', progressbar.FileTransferSpeed()],
-        maxval=total_size).start()
+        pbar = progressbar.ProgressBar(widgets=[
+            'Encoding: ', progressbar.AnimatedMarker(), progressbar.Percentage(
+            ), ' ', progressbar.Bar(marker='*'), ' ', progressbar.ETA(
+            ), ' ', progressbar.FileTransferSpeed()
+        ],
+                                       maxval=total_size).start()
 
         for c in cycle(('-', '\\', '|', '/')):
             res.wait(0.1)
@@ -159,7 +161,8 @@ if __name__ == '__main__':
                 while True:
                     status, filename, info = status_queue.get_nowait()
                     if 'ERROR' == status:
-                        print >>sys.stderr, "ERROR: %s (%s)" % (info, filename)
+                        print >> sys.stderr, "ERROR: %s (%s)" % (info,
+                                                                 filename)
 
                     bytes_processed += sizes[filename]
                     pbar.update(bytes_processed)
@@ -173,7 +176,7 @@ if __name__ == '__main__':
             pbar.update()
 
     except KeyboardInterrupt:
-        print >>sys.stderr, "exiting..."
+        print >> sys.stderr, "exiting..."
         pool.terminate()
         pool.join()
         print "done"
