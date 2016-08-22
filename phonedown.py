90dcb598cf8fa3009d329be234b11544e3859500
Marc Brinkmann <git@marcbrinkmann.de>
2016-08-22 14:26:42 +0200

    Turn into class-based app.

--- phonedown.py
+++ phonedown.py
@@ -46,9 +46,9 @@ parser.add_argument('--mp3gain', default='mp3gain')
 
 
 def get_out_path(args, full_path):
-    rel_path = os.path.relpath(full_path, args.source_folder)
+    rel_path = os.path.relpath(full_path, source_folder)
     base, ext = os.path.splitext(rel_path)
-    out_path = os.path.join(args.dest_folder, base + '.mp3')
+    out_path = os.path.join(dest_folder, base + '.mp3')
 
     return out_path
 
@@ -99,12 +99,12 @@ def convert_file(args, full_path):
         status_queue.put(('OK', full_path, out_path))
 
 
-def list_files(args):
-    for dirpath, dirnames, filenames in os.walk(args.source_folder):
+def list_files(source_folder, skip_existing, extensions):
+    for dirpath, dirnames, filenames in os.walk(source_folder):
         for fn in filenames:
             full_path = os.path.join(dirpath, fn)
             root, ext = os.path.splitext(full_path)
-            if not ext in args.extensions:
+            if ext not in extensions:
                 # ignore unknown extensions
                 continue
 
@@ -123,60 +123,102 @@ def ignore_sigint():
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
 
+class PhoneDown(object):
+    def __init__(self, source_folder, dest_folder, extensions, pool_size,
+                 apply_mp3gain, skip_existing, lame, flac, mp3gain):
+        # FIXME: lame_options
+        lame_options = []
+
+        sizes = {}
+        total_size = 0
+        bytes_processed = 0
+
+        for _, full_path in list_files(args):
+            filesize = os.stat(full_path).st_size
+            sizes[full_path] = filesize
+            total_size += filesize
+
+        print("Converting from %s to %s" % (source_folder, dest_folder))
+
+        if not sizes:
+            print("Nothing to be done")
+            sys.exit(0)
+
+        print("Using %d workers to process %.2fM" %
+              (args.pool_size, total_size / (1024 * 1024)))
+
+        pool = Pool(args.pool_size, ignore_sigint)
+
+        try:
+            res = pool.map_async(_convert_file_args, list_files(args))
+            # pbar = progressbar.ProgressBar(widgets=[
+            #     'Encoding: ', progressbar.AnimatedMarker(), progressbar.Percentage(
+            #     ), ' ', progressbar.Bar(marker='*'), ' ', progressbar.ETA(
+            #     ), ' ', progressbar.FileTransferSpeed()
+            # ],
+            #                                maxval=total_size).start()
+
+            for c in cycle(('-', '\\', '|', '/')):
+                res.wait(0.1)
+                try:
+                    while True:
+                        status, filename, info = status_queue.get_nowait()
+                        if 'ERROR' == status:
+                            print("ERROR: %s (%s)" % (info, filename),
+                                  file=sys.stderr)
+
+                        bytes_processed += sizes[filename]
+                        # pbar.update(bytes_processed)
+                except Empty:
+                    pass
+
+                if res.ready():
+                    # pbar.finish()
+                    break
+
+                # pbar.update()
+
+        except KeyboardInterrupt:
+            print("exiting...", file=sys.stderr)
+            pool.terminate()
+            pool.join()
+            print("done", file=sys.stderr)
+
+
+@click.command()
+@click.argument('source_folder')
+@click.argument('dest_folder')
+@click.option('--extensions',
+              '-e',
+              multiple=True,
+              default=('.flac', ),
+              help='Extension of files to convert, may be given multiple '
+              'times (default: .flac)')
+@click.option('--pool-size',
+              '-p',
+              type=int,
+              default=1 + cpu_count(),
+              help='Number of worker processes (default: # cores + 1)')
+@click.option('--mp3gain/--no-mp3gain',
+              'apply_mp3gain',
+              default=True,
+              help='Enable/disable mp3 gain (default: enabled)')
+@click.option('--skip/--no-skip',
+              'skip_existing',
+              default=True,
+              help='Skip already processed files (default: enabled)')
+@click.option('--lame',
+              default='lame',
+              help='Override lame binary (default: "lame")')
+@click.option('--flac',
+              default='flac',
+              help='Override flac binary (default: "flac")')
+@click.option('--mp3gain',
+              default='mp3gain',
+              help='Override mp3gain binary (default: "mp3gain")')
+def cli(*args, **kwargs):
+    PhoneDown(*args, **kwargs).run()
+
+
 if __name__ == '__main__':
-    args = parser.parse_args()
-
-    sizes = {}
-    total_size = 0
-    bytes_processed = 0
-
-    for _, full_path in list_files(args):
-        filesize = os.stat(full_path).st_size
-        sizes[full_path] = filesize
-        total_size += filesize
-
-    print("Converting from %s to %s" % (args.source_folder, args.dest_folder))
-
-    if not sizes:
-        print("Nothing to be done")
-        sys.exit(0)
-
-    print("Using %d workers to process %.2fM" % (args.pool_size,
-                                                 total_size / (1024 * 1024)))
-
-    pool = Pool(args.pool_size, ignore_sigint)
-
-    try:
-        res = pool.map_async(_convert_file_args, list_files(args))
-        # pbar = progressbar.ProgressBar(widgets=[
-        #     'Encoding: ', progressbar.AnimatedMarker(), progressbar.Percentage(
-        #     ), ' ', progressbar.Bar(marker='*'), ' ', progressbar.ETA(
-        #     ), ' ', progressbar.FileTransferSpeed()
-        # ],
-        #                                maxval=total_size).start()
-
-        for c in cycle(('-', '\\', '|', '/')):
-            res.wait(0.1)
-            try:
-                while True:
-                    status, filename, info = status_queue.get_nowait()
-                    if 'ERROR' == status:
-                        print("ERROR: %s (%s)" % (info, filename),
-                              file=sys.stderr)
-
-                    bytes_processed += sizes[filename]
-                    # pbar.update(bytes_processed)
-            except Empty:
-                pass
-
-            if res.ready():
-                # pbar.finish()
-                break
-
-            # pbar.update()
-
-    except KeyboardInterrupt:
-        print("exiting...", file=sys.stderr)
-        pool.terminate()
-        pool.join()
-        print("done", file=sys.stderr)
+    cli()
