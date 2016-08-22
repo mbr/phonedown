b3ac419eecd4d39e72cc5adb86bd0b1d2dfe167e
Marc Brinkmann <git@marcbrinkmann.de>
2016-08-22 14:44:55 +0200

    First working version.

--- phonedown.py
+++ phonedown.py
@@ -16,109 +16,6 @@ from mutagen import File
 status_queue = Queue()
 
 
-def folder(v):
-    if not os.path.exists(v):
-        raise ValueError('Does not exist: %s' % v)
-
-    if not os.path.isdir(v):
-        raise ValueError('Not a directory: %s' % v)
-
-    return os.path.abspath(v)
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument('source_folder', type=folder)
-parser.add_argument('dest_folder', type=folder)
-parser.add_argument('-e', '--extensions', nargs='+', default=['.flac'])
-parser.add_argument('-p', '--pool-size', type=int, default=1 + cpu_count())
-parser.add_argument('lame_options', nargs=argparse.REMAINDER)
-parser.add_argument('--no-mp3gain',
-                    action='store_false',
-                    dest='apply_mp3gain',
-                    default=True)
-parser.add_argument('--no-skip',
-                    action='store_false',
-                    dest='skip_existing',
-                    default=True)
-parser.add_argument('--lame', default='lame')
-parser.add_argument('--flac', default='flac')
-parser.add_argument('--mp3gain', default='mp3gain')
-
-
-def get_out_path(args, full_path):
-    rel_path = os.path.relpath(full_path, source_folder)
-    base, ext = os.path.splitext(rel_path)
-    out_path = os.path.join(dest_folder, base + '.mp3')
-
-    return out_path
-
-
-def convert_file(args, full_path):
-    try:
-        out_path = get_out_path(args, full_path)
-
-        lame_options = args.lame_options or ['--preset', 'standard', '-h']
-
-        metadata = FLAC(full_path)
-
-        try:
-            os.makedirs(os.path.dirname(out_path))
-        except OSError as e:
-            if e.errno != 17:
-                raise  # only raise if not "file exists" error
-
-        flac_p = subprocess.Popen(
-            [args.flac, '-s', '-d', '--stdout', full_path],
-            stdout=subprocess.PIPE,
-            preexec_fn=ignore_sigint)
-        lame_cmd = [args.lame] + lame_options + ['--quiet', '-', out_path]
-        lame_p = subprocess.Popen(lame_cmd,
-                                  stdin=flac_p.stdout,
-                                  preexec_fn=ignore_sigint)
-
-        flac_p.wait()
-        lame_p.wait()
-
-        # now apply gain
-        if args.apply_mp3gain:
-            mp3gain_cmd = [args.mp3gain, '-q', '-T', '-r', '-k', out_path]
-
-            subprocess.check_call(mp3gain_cmd, stdout=open('/dev/null', 'wb'))
-
-        # finally, correct the tags
-        id3data = File(out_path, easy=True)
-        for attr in ('title', 'artist', 'album', 'date', 'genre',
-                     'tracknumber'):
-            id3data[attr] = metadata.get(attr)
-        id3data.save()
-    except Exception as e:
-        status_queue.put(('ERROR', full_path, str(e)))
-        if os.path.exists(out_path):
-            os.unlink(out_path)
-    else:
-        status_queue.put(('OK', full_path, out_path))
-
-
-def list_files(source_folder, skip_existing, extensions):
-    for dirpath, dirnames, filenames in os.walk(source_folder):
-        for fn in filenames:
-            full_path = os.path.join(dirpath, fn)
-            root, ext = os.path.splitext(full_path)
-            if ext not in extensions:
-                # ignore unknown extensions
-                continue
-
-            if args.skip_existing and\
-               os.path.exists(get_out_path(args, full_path)):
-                continue
-
-            yield args, full_path
-
-
-def _convert_file_args(args):
-    return convert_file(*args)
-
-
 def ignore_sigint():
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
@@ -126,31 +23,110 @@ def ignore_sigint():
 class PhoneDown(object):
     def __init__(self, source_folder, dest_folder, extensions, pool_size,
                  apply_mp3gain, skip_existing, lame, flac, mp3gain):
-        # FIXME: lame_options
-        lame_options = []
+        self.lame_options = []
 
+        self.source_folder = source_folder
+        self.dest_folder = dest_folder
+        self.extensions = extensions
+        self.pool_size = pool_size
+        self.apply_mp3gain = apply_mp3gain
+        self.skip_existing = skip_existing
+
+        self.lame = lame
+        self.flac = flac
+        self.mp3gain = mp3gain
+
+    def list_files(self):
+        for dirpath, dirnames, filenames in os.walk(self.source_folder):
+            for fn in filenames:
+                full_path = os.path.join(dirpath, fn)
+                root, ext = os.path.splitext(full_path)
+                if ext not in self.extensions:
+                    # ignore unknown extensions
+                    continue
+
+                if self.skip_existing and os.path.exists(self.get_out_path(
+                        full_path)):
+                    continue
+
+                yield full_path
+
+    def get_out_path(self, full_path):
+        rel_path = os.path.relpath(full_path, self.source_folder)
+        base, ext = os.path.splitext(rel_path)
+        out_path = os.path.join(self.dest_folder, base + '.mp3')
+
+        return out_path
+
+    def convert_file(self, full_path):
+        try:
+            out_path = self.get_out_path(full_path)
+            lame_options = self.lame_options or ['--preset', 'standard', '-h']
+
+            metadata = FLAC(full_path)
+
+            try:
+                os.makedirs(os.path.dirname(out_path))
+            except OSError as e:
+                if e.errno != 17:
+                    raise  # only raise if not "file exists" error
+
+            flac_p = subprocess.Popen(
+                [self.flac, '-s', '-d', '--stdout', full_path],
+                stdout=subprocess.PIPE,
+                preexec_fn=ignore_sigint)
+            lame_cmd = [self.lame] + lame_options + ['--quiet', '-', out_path]
+            lame_p = subprocess.Popen(lame_cmd,
+                                      stdin=flac_p.stdout,
+                                      preexec_fn=ignore_sigint)
+
+            flac_p.wait()
+            lame_p.wait()
+
+            # now apply gain
+            if self.apply_mp3gain:
+                mp3gain_cmd = [self.mp3gain, '-q', '-T', '-r', '-k', out_path]
+
+                subprocess.check_call(mp3gain_cmd,
+                                      stdout=open('/dev/null', 'wb'))
+
+            # finally, correct the tags
+            id3data = File(out_path, easy=True)
+            for attr in ('title', 'artist', 'album', 'date', 'genre',
+                         'tracknumber'):
+                id3data[attr] = metadata.get(attr)
+            id3data.save()
+        except Exception as e:
+            status_queue.put(('ERROR', full_path, str(e)))
+            if os.path.exists(out_path):
+                os.unlink(out_path)
+        else:
+            status_queue.put(('OK', full_path, out_path))
+
+    def run(self):
         sizes = {}
         total_size = 0
         bytes_processed = 0
 
-        for _, full_path in list_files(args):
+        for full_path in self.list_files():
             filesize = os.stat(full_path).st_size
             sizes[full_path] = filesize
             total_size += filesize
 
-        print("Converting from %s to %s" % (source_folder, dest_folder))
+        print("Converting from %s to %s" % (self.source_folder,
+                                            self.dest_folder))
 
         if not sizes:
             print("Nothing to be done")
             sys.exit(0)
 
         print("Using %d workers to process %.2fM" %
-              (args.pool_size, total_size / (1024 * 1024)))
+              (self.pool_size, total_size / (1024 * 1024)))
 
-        pool = Pool(args.pool_size, ignore_sigint)
+        pool = Pool(self.pool_size, ignore_sigint)
 
         try:
-            res = pool.map_async(_convert_file_args, list_files(args))
+            res = pool.map_async(self.convert_file, self.list_files())
             # pbar = progressbar.ProgressBar(widgets=[
             #     'Encoding: ', progressbar.AnimatedMarker(), progressbar.Percentage(
             #     ), ' ', progressbar.Bar(marker='*'), ' ', progressbar.ETA(
@@ -186,12 +162,15 @@ class PhoneDown(object):
 
 
 @click.command()
-@click.argument('source_folder')
-@click.argument('dest_folder')
+@click.argument('source_folder',
+                type=click.Path(exists=True,
+                                dir_okay=True,
+                                file_okay=False))
+@click.argument('dest_folder', type=click.Path(dir_okay=True, file_okay=False))
 @click.option('--extensions',
               '-e',
               multiple=True,
-              default=('.flac', ),
+              default=['.flac'],
               help='Extension of files to convert, may be given multiple '
               'times (default: .flac)')
 @click.option('--pool-size',
